65530f80091ad0378a77b2d4c2c96466d33b4e8f
Cayetano Cura <ccjspark@gmail.com>
2023-09-15 11:01:26 -0500

    added destructuring exercise to object-enhancement folder

--- html-javascript-practice/app.js
+++ html-javascript-practice/app.js
@@ -1,8 +1,20 @@
-function createAnimal(species, verb, noise) {
-    return {
-        species,
-        [verb]() {
-            return noise;
-        }
-    };
-}
+// let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+// let {numPlanets, yearNeptuneDiscovered} = facts;
+
+// let planetFacts = {
+//     numPlanets: 8,
+//     yearNeptuneDiscovered: 1846,
+//     yearMarsDiscovered: 1659
+//   };
+
+//   let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+//   let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+//     "Raindrops on roses",
+//     "whiskers on kittens",
+//     "Bright copper kettles",
+//     "warm woolen mittens",
+//     "Brown paper packages tied up with strings"
+//   ]
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
